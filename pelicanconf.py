b21fd441158d226d01b941af565c03af8f335ad2
Nolan Feeny <30299375+nolanfeeny@users.noreply.github.com>
2018-06-04 13:14:57 -0400

    Add files via upload

---
+++ pelicanconf.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'authorname'
+SITENAME = "Author Name"
+SITEURL = ''
+
+PATH = 'content'
+
+
+#PLUGIN_PATHS=["./plugins"]
+#PLUGINS=['ipynb.markup']
+#MARKUP = { 'md', 'ipynb'}
+
+TIMEZONE = 'America/Detroit'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+#LINKS = (('Pelican', 'http://getpelican.com/'),
+#         ('Python.org', 'http://python.org/'),
+#         ('Jinja2', 'http://jinja.pocoo.org/'),
+#         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+#SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+
+# Comment following line if you want the default theme
+THEME = 'themes/tuxlite2'
+
+DISPLAY_PAGES_ON_MENU = True
+DISPLAY_CATEGORIES_ON_MENU = True
+
+#INDEX_SAVE_AS = 'about.html'
+#PAGE_SAVE_AS = 'about2.html'
+#PAGE_URL = 'about2.html'
+
+# Provides menu items, which come before pages / categories
+#MENUITEMS = [('News','file:///Users/cmse/Desktop/pelican/backupblog/output/category/#news.html')]
